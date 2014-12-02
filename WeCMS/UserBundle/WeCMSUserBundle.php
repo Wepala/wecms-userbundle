9dfda0a615e808727e7389478aa70bda84b9d7ef
Akeem <akeem.philbert@wepala.com>
2014-12-02 09:56:59 -0400

    added version and license info

--- WeCMS/UserBundle/WeCMSUserBundle.php
+++ WeCMS/UserBundle/WeCMSUserBundle.php
@@ -1,1 +1,26 @@
 <?php
+
+/*
+ * This file is part of the WeCMSUserBundle package.
+*
+* (c) Wepala LLC 
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace WeCMS\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Akeem Philbert <akeem.philbert@wepala.com>
+ */
+class WeCMSUserBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
